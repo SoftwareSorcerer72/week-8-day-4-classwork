dfd3462859ff06809c53efd02b17f6fb0ebde8d5
Your NameSoftwareSorcer72 <you@example.commikewelborn72@gmail.com>
2024-04-12 12:15:35 -0400

    Updated classwork with more content.

--- src/Widget/index.ts
+++ src/Widget/index.ts
@@ -1,8 +1,13 @@
 import Canvas from './Canvas';
 import Component from './Component';
-
+import { LeftLeaningContainer } from './containers';
+import { RightLeaningContainer } from './containers';
+import { CircleContainer } from './containers';
 
 export {
     Canvas,
-    Component
+    Component,
+    LeftLeaningContainer,
+    RightLeaningContainer,
+    CircleContainer
 }
